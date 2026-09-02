a595f7501bcacd474d5e51267d035a511a4b70db
itzscaraa <aprilyonardiyt@gmail.com>
2026-09-02 19:21:43 +0700

    versi 2

--- index.php
+++ index.php
@@ -6,6 +6,28 @@
     <title>Document</title>
 </head>
 <body>
+    <?php 
+    $nama = "April";
+    $umur = 19;
+    $status = false;
     
+    // nama saya April, umur saya 19 tahun dan saya belum menikah
+
+    if ($status == false) {
+        echo "nama saya ". $nama . " , " . "umur saya " . $umur . " tahun dan saya belum menikah" ;
+    } else {
+        echo "nama saya ". $nama . " , " . "umur saya " . $umur . " tahun dan saya sudah menikah" ;
+    }
+
+    echo "<br>";
+
+    for ($i=1; $i < 10; $i+=2) {
+        echo $i;
+    }
+
+    ?>
+
+    
+
 </body>
 </html>
